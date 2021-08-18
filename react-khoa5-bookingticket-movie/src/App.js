22f7015c308b3ea98b5ce4e5be564a066b89937e
NguyenTien2401 <hoangtienflytom@gmail.com>
2021-08-18 10:53:12 +0700

    Day1BookingMovie

---
+++ react-khoa5-bookingticket-movie/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="shadow-sm" style={{width : '20%' , height:"50px"}} >sdfasdfas</div>
+
+    </div>
+  );
+}
+
+export default App;
